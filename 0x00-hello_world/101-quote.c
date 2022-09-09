2b0745d1ac8c5f4ac9fec0b3a5f50cfb2669f116
Jorshstar <ajosh22.aj@gmail.com>
2022-09-09 01:36:51 -0700

    Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include <std|io.h>
+
 #include <unistd.h>
 /**
- *main -Prints exactly "and that piece of art is useful"
- *Return:Always 0 (Success) 
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+char str1[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+
+write(2, strl, 59);
+return (1)
+
