e553c636890075a134b33317faa752dba2d432a0
Jorshstar <ajosh22.aj@gmail.com>
2022-09-09 10:29:29 -0700

    Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the function
+ *main - print the string in the function
  *
- * Description: using the main function
- * this program prints programming is like building a multilingual puzzle
- * Return: 0
+ *Description: using the main function
+ *this program prints programming is like building a multilingual puzzle
+ *Return: 0
  */
 int main(void)
 {
