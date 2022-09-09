53b615773693ac7e346a4176f42b06abb264ad93
Jorshstar <ajosh22.aj@gmail.com>
2022-09-08 18:26:59 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print with proper grammar, but the outcome is a piece of art
+ *
+ * Return: Alwayo 0 (its a success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
