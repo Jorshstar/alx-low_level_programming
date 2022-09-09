eb0bb06f461c0a608b68c43da892a51c4556f56a
Jorshstar <ajosh22.aj@gmail.com>
2022-09-08 19:37:17 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
-/*
- * main - Prints a text
- * 
- *  Return: Always 1 (Success) 
+/**
+ * main - Prints exactly and that piece of art is useful
+ * Return: Always 0 (Success) 
  */
-int main(void) 
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
