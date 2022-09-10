6578bb014c360c5f399c48720fdbe5615edc5864
Jorshstar <ajosh22.aj@gmail.com>
2022-09-10 03:12:19 -0700

    Tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * * main - let's keep coding
- * * Description: using the main function
- * * this program prints all possible different combinations of three digits
- * * Return: 0
- * */
+ * main - print if the number is positive, zero, or  negative
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
 {
 	int i, o, u;
