e3a8f70bbfc1ac86dc3107de7e70aa062ba9de42
Jorshstar <ajosh22.aj@gmail.com>
2022-09-10 03:20:28 -0700

    Tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-
 /**
  * main - print if the number is positive, zero, or  negative
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
+ *
+ *
  */
 int main(void)
 {
 	int i, o, u;
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (o = i + 1; o <= '9'; o++)
