deeccc5a760ec488d03072ed74c0216d3d734538
Jorshstar <ajosh22.aj@gmail.com>
2022-09-10 04:06:33 -0700

    Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-
 /**
- *main - print the string in the function
+ * main - Entry point
  *
- *Description: using the main function
- *this program prints programming is like building a multilingual puzzle
- *Return: 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(a));
-	printf("size of an int : %ld byte(s)\n", sizeof(b));
-	printf("size of a long int: %ld byte(s)\n", sizeof(c));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(d));
-	printf("size of a float: %ld byte(s)\n", sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
