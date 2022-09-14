c4d445ed31049a4910ee34d26258b3ef239c8f5b
Jorshstar <ajosh22.aj@gmail.com>
2022-09-13 20:43:06 -0700

    Tasks

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * * main - I just dey code dey trabaye
+ * * main - C is 1 of the hardest, its 4 and am still coding
  * description: using the main function, this program prints
  * the sum of the even values of a fibonacci
  * Return: 0
