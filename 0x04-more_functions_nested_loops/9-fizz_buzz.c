221595ceef94b7c6239d35d14d809908af333ba4
Jorshstar <ajosh22.aj@gmail.com>
2022-09-15 18:33:27 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number from 1 to 100
+ * we are buzzing out gradually
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("Buzz\n");
+	return (0);
+}
