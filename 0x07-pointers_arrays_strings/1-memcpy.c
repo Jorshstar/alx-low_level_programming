0f41eb15559c9ce44be8031feffc8c3725107f4b
Jorshstar <ajosh22.aj@gmail.com>
2022-09-26 15:04:10 -0700

    more strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - The function find first occurence of a char in a string
+ * @s: The function for  string to be searched
+ * @c: The function for character to be found
+ * Return: Pointer to the first occurence
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (NULL);
+}
