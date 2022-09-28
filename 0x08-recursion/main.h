0957cb6bf0df295808dc73dab876b44647d69367
Jorshstar <ajosh22.aj@gmail.com>
2022-09-28 14:09:41 -0700

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,11 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-/**
- * char prototypes
- * int prototypes
- */
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
