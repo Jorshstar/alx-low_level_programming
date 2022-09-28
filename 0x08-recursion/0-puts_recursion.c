25939f25a4781b614d4f24090df1cd25f42c1b81
Jorshstar <ajosh22.aj@gmail.com>
2022-09-28 14:03:05 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string 
+ * @s: paramater for string to be printed
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
+}
