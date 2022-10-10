f5c99797e727de912ae75b73f0c9a19eed932fd3
Jorshstar <ajosh22.aj@gmail.com>
2022-10-10 10:17:07 -0700

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef ABS_X
-#define ABS_X
-#define ABS(X) ((X < 0) ? (X * -1) : (X))
+#ifndef FUNCITION_LIKE_MACRO_H
+#define FUNCITION_LIKE_MACRO_H
+
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
+
 #endif
