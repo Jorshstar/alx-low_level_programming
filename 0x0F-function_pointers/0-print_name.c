4dbdf6103a5694e1683c1f0c7f221a309f3fcc1c
Jorshstar <ajosh22.aj@gmail.com>
2022-10-12 14:11:28 -0700

    Function Pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stddef.h>
+
 /**
- * print_name - A function thats prints a name
- * @name: char to display to the stdout
- * @f: A pointer function
- *
- * Return: No return cause we've declared VOID as R_TYPE
-*/
+ * * print_name - this function prints a name
+ * * @name: This is the name to be printed, a char
+ * @f: This is a pointer to a func
+ * Return: Null Void
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f) /* if any is NULL */
+	if (name == NULL || f == NULL)
 		return;
-
 	f(name);
+}
